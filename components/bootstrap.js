23febd8fe0ae069ff9b8b430359914f08bae2b9d
zackr <reiter.z@husky.neu.edu>
2019-03-21 17:40:43 -0400

    close link tag

--- components/bootstrap.js
+++ components/bootstrap.js
@@ -3,7 +3,7 @@ import Head from 'next/head';
 function Bootstrap() {
   return (
     <Head>
-      <link rel="stylesheet" href="/static/bootstrap-4.3.1-dist/bootstrap-4.3.1-dist/css/bootstrap.min.css">
+      <link rel="stylesheet" href="/static/bootstrap-4.3.1-dist/bootstrap-4.3.1-dist/css/bootstrap.min.css"></link>
     </Head>
   );
 }
